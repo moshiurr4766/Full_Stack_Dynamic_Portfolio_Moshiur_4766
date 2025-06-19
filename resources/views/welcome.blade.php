8baa3313cccdaf61f755c8b9126bf7992aaa7c80
moshiur4766 <rahman15-4766@diu.edu.bd>
2025-06-19 15:07:32 +0600

    alert with javaScript

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <div class="container">
             <nav>
                 <div class="logo">🔴</div>
-                <ul>
+                <ul class = "manu">
                     <li><a href="#">Home</a></li>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Portfolio</a></li>
@@ -121,5 +121,31 @@
             <p>Email: natalie@example.com</p>
         </div>
     </footer>
+
+
+    <script>
+
+    const menu = document.querySelector('.manu');
+
+    if (menu.children.length > 0) {
+        for (let i = 0; i < menu.children.length; i++) {
+            menu.children[i].addEventListener('click', function (event) {
+                alert("Clicked");
+            });
+        }
+    }
+
+    const menu1 = document.querySelector('.experience-grid');
+
+    if (menu1.children.length > 0) {
+        for (let i = 0; i < menu1.children.length; i++) {
+            menu1.children[i].addEventListener('click', function (event) {
+                alert("Clicked");
+            });
+        }
+    }
+
+</script>
+
 </body>
 </html>
